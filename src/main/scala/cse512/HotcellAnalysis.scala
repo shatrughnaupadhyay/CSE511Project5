93caab9aeb967799797939c1f74baa4a86a21b57
shatrughnaupadhyay <86815631+shatrughnaupadhyay@users.noreply.github.com>
2021-07-01 20:29:27 -0700

    Update HotcellAnalysis.scala

--- src/main/scala/cse512/HotcellAnalysis.scala
+++ src/main/scala/cse512/HotcellAnalysis.scala
@@ -46,47 +46,28 @@ def runHotcellAnalysis(spark: SparkSession, pointPath: String): DataFrame =
   val numCells = (maxX - minX + 1)*(maxY - minY + 1)*(maxZ - minZ + 1)
 
   // YOU NEED TO CHANGE THIS PART
-  // Calculate X_bar = average number of pick-up points in one cell
-  val x_mean:Double = pickupInfo.count().toDouble / numCells.toDouble
+  pickupInfo.createOrReplaceTempView("allPickUps")
+  spark.udf.register("CheckContain",(x:Int, y:Int, z:Int)=>(HotcellUtils.CheckContain(x,y,z)))
+  val pointsInCube = spark.sql("select x,y,z from allPickUps where CheckContain(x, y, z)")
+  pointsInCube.createOrReplaceTempView("pointsInCube")
 
-  // Calculate attribute value x for each cell. x_i = count of points within this cell i
-  var pickupCell = pickupInfo.groupBy("x", "y", "z").count().withColumnRenamed("count", "attr")  // rename x_i to attr_i to differentiate from coordinate
-  
-  // add x_i squred to each cell
-  pickupCell = pickupCell.withColumn("attr_2", expr("attr*attr"))
-  pickupCell.show()
+  val points = spark.sql("select count(*) as totalPoint from pointsInCube")
+  points.createOrReplaceTempView("points")
+  val pointsNumber = points.first().getLong(0)
 
-  // calculate S
-  var S:Double = pickupCell.agg(sum("attr_2")).first.getLong(0).toDouble
-  S = sqrt(S / numCells.toDouble - pow(x_mean, 2))
-  println("S="+S)
 
-  // add two more columns to record neighboring cell's attribute value
-  pickupCell = pickupCell.drop("attr_2") // no need this attr^2 col
-  var neighborCellAttr = pickupCell.as("df1").join(pickupCell.as("df2"),
-    (col("df1.x") === col("df2.x") - 1 || col("df1.x") === col("df2.x") + 1 || col("df1.x") === col("df2.x")) &&
-    (col("df1.y") === col("df2.y") - 1 || col("df1.y") === col("df2.y") + 1 || col("df1.y") === col("df2.y")) &&
-    (col("df1.z") === col("df2.z") - 1 || col("df1.z") === col("df2.z") + 1 || col("df1.z") === col("df2.z"))// &&
-    //!(col("df1.x") === col("df2.x") && col("df1.y") === col("df2.y") && col("df1.z") === col("df2.z")),
-    ,"inner").select(col("df1.x").as("x"), col("df1.y").as("y"), col("df1.z").as("z"), col("df2.attr").as("attr"))
-  println("displaying neighboring cell attribute value")
-  neighborCellAttr.show()
+  val countCube = spark.sql("select count(*) as totalPoints, power(count(*), 2) as squarePoints ,  x, y, z from pointsInCube group by x,y,z")
+  countCube.createOrReplaceTempView("countCube")
+  val cubesNumber = countCube.count()
 
-  // calculate sigma_j=1_N(x_j), and N
-  val neighborCountAgg = neighborCellAttr.groupBy("x", "y", "z").agg(count("attr").as("N"), sum("attr").as("sum_attr"))
-  println("displaying aggregated neighboring cell count and attribute value")
-  neighborCountAgg.show()
+  println(pointsNumber)
+  println(cubesNumber)
+  val avgX = pointsNumber.toDouble / cubesNumber.toDouble
 
-  // calculate G
-  spark.udf.register("calculateG", (attr_agg:Int, N:Int) => ((HotcellUtils.calculateG(attr_agg.toDouble, N.toDouble, numCells.toDouble, x_mean, S))))
-  neighborCountAgg.createOrReplaceTempView("neighborCountAgg")
-  val cellGScore = spark.sql("select x, y, z, calculateG(sum_attr, N) as G from neighborCountAgg order by G DESC, x DESC, y DESC, z DESC")
-  println("displaying G score")
-  cellGScore.show()
-
-  val ret = cellGScore.select(col("x"), col("y"), col("z")) // YOU NEED TO CHANGE THIS PART
-  println("displaying ret")
-  ret.show()
-  return ret.coalesce(1)
+  val squarePointsDf = spark.sql("select sum(squarePoints) from countCube")
+  val squarePoints = squarePointsDf.first().getDouble(0)
+  val largeS = sqrt((squarePoints / cubesNumber) - (pow(avgX, 2)))
+  println(largeS)
+  return pointsInCube
 }
 }
